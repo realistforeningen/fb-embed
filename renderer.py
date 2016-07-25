67fa9643cb548ac94217c75c568b6b10b9c88875
Magnus Holm <judofyr@gmail.com>
2016-07-25 15:39:23 +0200

    Move CSS files to files-directory

--- renderer.py
+++ renderer.py
@@ -9,13 +9,12 @@ env = Environment(
     loader=FileSystemLoader(base_dir + "/html")
 )
 
-styles = {}
-for fname in os.listdir(base_dir + "/css"):
-    name = os.path.splitext(fname)[0]
-    with codecs.open(base_dir + "/css/" + fname, encoding='utf-8') as f:
-        styles[name] = f.read()
+files = {}
+for fname in os.listdir(base_dir + "/files"):
+    with codecs.open(base_dir + "/files/" + fname, encoding='utf-8') as f:
+        files[fname] = f.read()
 
 def render(template_name, **kwargs):
     tmpl = env.get_template(template_name)
-    return tmpl.render(styles=styles, **kwargs)
+    return tmpl.render(files=files, **kwargs)
 
