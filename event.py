e78ca8a187ec639a8f7f44e1d847e65da4a2f95c
Magnus Holm <judofyr@gmail.com>
2016-08-09 10:19:07 +0200

    Facebook: Show all events

--- event.py
+++ event.py
@@ -9,13 +9,12 @@ import config
 
 graph = GraphAPI(access_token=config.ACCESS_TOKEN, version="2.5")
 
-def get_latest_event_raw():
+def get_latest_events_raw():
     id = config.EVENT_PAGE + "/events"
     return graph.get_object(
         id,
-        limit=1,
-        fields="id,name,start_time,attending_count,interested_count"
-    )
+        fields="id,name,start_time,end_time,attending_count,interested_count"
+    )['data']
 
 
 EVENT_PICTURE_RE = re.compile('coverPhotoImg(.*?)src="([^"]+)')
@@ -29,23 +28,32 @@ def get_full_event_picture(url):
         return match.group(2).replace('&amp;', '&')
 
 class Event(object):
-    def __init__(self, obj):
-        data = obj['data'][0]
+    def __init__(self, data):
         self.id = data['id']
         self.url = u"https://www.facebook.com/events/" + self.id
         self.name = data['name']
         self.start_time = arrow.get(data['start_time'])
+        if 'end_time' in data:
+            self.end_time = arrow.get(data['end_time'])
         self.attending_count = data['attending_count']
         self.interested_count = data['interested_count']
 
-def get_latest_event():
-    event = Event(get_latest_event_raw())
-    event.picture_url = get_full_event_picture(event.url)
-    return event
+    def fetch_full_picture(self):
+        self.picture_url = get_full_event_picture(self.url)
+
+    def is_active(self):
+        if hasattr(self, 'end_time'):
+            return self.end_time > arrow.now()
+        else:
+            return self.start_time > arrow.now()
 
 def aws_func(event, context):
-    event = get_latest_event()
-    html = render('event.html', event=event)
+    events = [Event(data) for data in get_latest_events_raw()]
+    events = filter(lambda e: e.is_active(), events)
+    events = events[::-1]
+    main_event = events.pop(0)
+    main_event.fetch_full_picture()
+    html = render('event.html', main_event=main_event, rest_events=events)
     return {'html': html}
 
 if __name__ == '__main__':
